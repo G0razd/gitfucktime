b207aacd3cbf23d93da415b6ca63322f84fc28db
Lukáš Gorazd Hrodek <lukas.gorazd.hrodek@gmail.com>
2026-01-29 06:29:40 +0100

    Build: Migrate to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
 from setuptools import setup
 
-setup(
-    name='gitfucktime',
-    version='0.1.0',
-    py_modules=['gitfucktime'],
-    install_requires=[
-        'rich',
-    ],
-    entry_points={
-        'console_scripts': [
-            'gitfucktime=gitfucktime:main',
-        ],
-    },
-)
+setup()
